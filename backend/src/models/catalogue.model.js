f644ec84fae0c9ce166fdcf09d4c6f2369b2e0e4
Philip Osztromok <emubantam@gmail.com>
2026-03-24 13:48:21 +0000

    drizzle

--- backend/src/models/catalogue.model.js
+++ backend/src/models/catalogue.model.js
@@ -17,7 +17,8 @@ import { pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 
 const itemsTable = pgTable("items", { 
     id: uuid().primaryKey(),
-    catagory: varchar( { length: 45 } ).notNull(),
+    category: varchar( { length: 45 } ).notNull(),
+    tags: text().default(" "),
 })
 
 export default itemsTable;
