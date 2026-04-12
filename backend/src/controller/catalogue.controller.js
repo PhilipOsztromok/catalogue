d92793c236a18b8c71f10ae9b347fcebcdd909b7
rimuru <emubantam@gmail.com>
2026-04-12 11:24:08 +0100

    thunderclient

--- backend/src/controller/catalogue.controller.js
+++ backend/src/controller/catalogue.controller.js
@@ -1,6 +1,28 @@
-export const uploadFiles = async  ( req,res ) => {
+import { db } from '../db/index.js';
+import { items } from '../models/catalogue.model.js';
+
+export const createItems = async ( req,res,next ) => {
     try {
-        const data = req.body;
+        const data = { ...req.body }
+        const newItem = { 
+            title: data.title,
+            category: data.category,
+            description: data.description || '',
+         }
+    const [ created ] = await db.insert(items).values(newItem).returning()
+
+    res.status(201).json( { created, message:"Item inserted sucessfully!" } )
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateItems = async ( req,res,next ) => {
+    try {
+        const data = { ...req.body }
+        const id = { ...req.params }
+        
     } catch (error) {
         
     }
--- backend/src/models/catalogue.model.js
+++ backend/src/models/catalogue.model.js
@@ -13,12 +13,18 @@ const catalogueSchema = new mongoose.Schema( {
 
 const catalogue = mongoose.model("item", catalogueSchema); */
 
+import { timestamp } from "drizzle-orm/gel-core";
 import { pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 
-const itemsTable = pgTable("items", { 
+const items = pgTable("items", { 
     id: uuid().primaryKey(),
+    title: varchar( { length: 100 } ),
+    description: text().default(" "),
     category: varchar( { length: 45 } ).notNull(),
-    tags: text().default(" "),
+    tags: text().default("[]"),
+    filepath: text().default(""),
+    addedAt: timestamp().defaultNow(),
+    updatedAt: timestamp().defaultNow(),
 })
 
-export default itemsTable;
+export default items;
