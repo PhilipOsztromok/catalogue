781a142b5ef98ce5612a60d24ecc23b0de7040a3
Philip Osztromok <emubantam@gmail.com>
2026-05-03 17:44:14 +0100

    modified helper.js

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,3 +1,4 @@
+import { json } from 'node:stream/consumers';
 import {upload} from '../middleware/upload.js'
 
 export function deleteFile(path) {
@@ -11,4 +12,30 @@ export function deleteFile(path) {
 export function buildRelativePath(file) {
   const rel = file.destination.replace(upload,'').replace(/^[\\/]/, '');
   return `${rel}/${file.filename}`;
+}
+
+export function parseTags(tag) {
+    if(!tag) return [];
+    if(Array.isArray(tag))
+        return tag;
+    try {
+        const parse = JSON.parse(tag);
+        return Array.isArray(parse) ? parse : [];
+    } catch (error) {
+        return tag.split(",").map((element) => 
+            element.trim()
+        )
+    }
+}
+
+export function serializeTags(tag) {
+    return JSON.stringify(Array.isArray(tag) ? tag : [] );
+}
+
+export function deserializeTags(tag) {
+    try {
+        return JSON.parse(tag || "[]");
+    } catch (error) {
+        return [];
+    }
 }
